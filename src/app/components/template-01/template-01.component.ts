c70209043cf00578220763b3c860433ab956a373
Ahmed Peter <peter@cloud9.ng>
2018-06-11 10:53:10 +0100

    added resume model and modified dashboard template

--- src/app/components/template-01/template-01.component.ts
+++ src/app/components/template-01/template-01.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+// user model
+import { UserInformation } from '../../model/userinfo';
+
 
 @Component({
   selector: 'app-template-01',
   templateUrl: './template-01.component.html',
   styleUrls: ['./template-01.component.css']
 })
-export class Template01Component implements OnInit {
+export class Template01Component {
 
+  userinfo: UserInformation = new UserInformation();
+
+  
   constructor() { }
 
-  ngOnInit() {
   }
 
-}
+//   ngOnInit() {
+  	
+
+// };
+
